26eff76f0543f24c83f61e985d4c1db65baaba8e
˹ʏᴜᴍɪ ꭙ˼ <146187449+YumiXBot@users.noreply.github.com>
2024-02-16 14:17:41 +0530

    Update console.py

--- AdityaHalder/console.py
+++ AdityaHalder/console.py
@@ -52,7 +52,7 @@ PM_GUARD_LIMIT = int(getenv("PM_GUARD_LIMIT", 5))
 
 
 # USERBOT DEFAULT IMAGE
-USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://graph.org/file/1217cb1e402b99fa47fdf.jpg")
+USERBOT_PICTURE = getenv("USERBOT_PICTURE", "https://te.legra.ph/file/81a524e7e53f1a774cd8c.jpg")
 
 
 
